68023699f8c25d23bdbc8eded7de5580abfe82a6
Ayan Choudhary <darkrider251099@gmail.com>
2020-05-21 06:46:54 +0530

    middlewares: Add token validation middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,18 @@ import logger from "morgan";
 import bodyparser from "body-parser";
 import cors from "cors";
 import config from "./config";
+import { ValidateToken, ValidateAdminToken } from "./middlewares";
 
 /**
  * Registers routes to the app instance
  * @param app Express application
  */
 const registerRoutes = (app: express.Express): void => {
-	app.get("/", (_req, res) => {
+	app.get("/", ValidateToken, (_req, res) => {
 		res.status(200).json({ message: "hello world" });
+	});
+	app.get("/admin", ValidateAdminToken, (_req, res) => {
+		res.status(200).json({ message: "something" });
 	});
 };
 
